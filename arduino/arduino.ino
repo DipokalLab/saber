4bae8fecdf9e7db79e8e3f5c711301c52a718ae0
Huh Hyeongjun <hhj@devent.kr>
2025-07-25 19:47:58 +0900

    feat: add arduino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,23 +1,56 @@
-String receivedString;
+#include "Arduino_BMI270_BMM150.h"
+#include <MadgwickAHRS.h>
+#include <ArduinoJson.h>
+
+Madgwick filter;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200); 
+  
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  filter.begin(100); 
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    receivedString = Serial.readStringUntil('\n');
+  float ax, ay, az;
+  float gx, gy, gz;
+  float mx, my, mz;
 
-    if (receivedString == "LED_ON") {
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.println("Arduino: LED is now ON.");
-    } else if (receivedString == "LED_OFF") {
-      digitalWrite(LED_BUILTIN, LOW);
-      Serial.println("Arduino: LED is now OFF.");
-    } else {
-      Serial.print("Arduino received: ");
-      Serial.println(receivedString);
-    }
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable() && IMU.magneticFieldAvailable()) {
+    
+    IMU.readAcceleration(ax, ay, az);
+    IMU.readGyroscope(gx, gy, gz);
+    IMU.readMagneticField(mx, my, mz);
+
+    filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
+    
+
+    float pitch = filter.getPitch();
+    float roll = filter.getRoll();
+    float yaw = filter.getYaw();
+
+    StaticJsonDocument<512> doc;
+
+    // JsonObject rot = doc.createNestedObject("rot");
+    // rot["pitch"] = pitch;
+    // rot["roll"] = roll;
+    // rot["yaw"] = yaw;
+
+    JsonObject gyro = doc.createNestedObject("gyro");
+    gyro["x"] = gx;
+    gyro["y"] = gy;
+    gyro["z"] = gz;
+
+    // JsonObject accel = doc.createNestedObject("accel");
+    // accel["x"] = ax;
+    // accel["y"] = ay;
+    // accel["z"] = az;
+
+    serializeJson(doc, Serial);
+    Serial.println();
   }
 }
